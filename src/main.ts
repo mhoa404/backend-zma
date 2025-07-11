f90760e961812c730cbf8ea6587f97352a876ffa
mhoa404 <mhoa.workspace@gmail.com>
2025-07-11 12:30:14 +0700

    Open CORS

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000);
 }
 
-bootstrap();
+void bootstrap();
